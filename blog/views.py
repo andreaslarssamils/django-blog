87764c1e72d517c813b889b9bc4683ff1a361444
andreaslarssamils <larssamils.andreas@gmail.com>
2025-08-15 08:10:53 +0200

    Add blog URLs and views for post listing; create post_list template

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.views import generic
+from .models import Post
 
-def home(request):
-    return HttpResponse("Welcome to the blog!")
+
+# Create your views here.
+class PostList(generic.ListView):
+    model = Post
