0cc30f55fd5f9af7a25d507ba9de20b5382c67d7
andreaslarssamils <larssamils.andreas@gmail.com>
2025-08-16 09:08:37 +0200

    updates the html files and add inherited styles

--- blog/views.py
+++ blog/views.py
@@ -5,4 +5,8 @@ from .models import Post
 
 # Create your views here.
 class PostList(generic.ListView):
-    model = Post
+    queryset = Post.objects.filter(status=1)
+    template_name = "blog/index.html"
+    # queryset = Post.objects.all().order_by("-created_on")
+    #queryset = Post.objects.filter(author=3)
+    # model = Post
